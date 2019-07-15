63b32b30531eb654fda02a356974eaeb059d4ba3
Dinkie <david.churchill72@gmail.com>
2019-07-15 15:13:40 +0100

    Moving

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ var swan;
 var hill;
 var bkgd;
 var maxScore = 770;
-var maxUp = maxScore*Math.sin(16.2);
-var maxRight = maxScore*Math.cos(16.2);
+var maxUp = maxScore*Math.sin(16);
+var maxRight = maxScore*Math.cos(16);
 var currentScore = 0;
 
 var currentQuestion = 0;
@@ -22,14 +22,25 @@ $(document).ready(function(){
 
   displayQuestion();
   $('button').click(function(){
-    var amount = parseInt(this.innerHTML);
+    var amount = parseInt(this.innerHTML.slice(1));
+    move(amount);
   });
 });
 
+function move(amount){
+  console.log(amount);
+  var swanPos = swan.position();
+  var hillPos = hill.position();
+  var time = 2000*(amount/(maxScore/5));
+  swan.animate({ top: swanPos.top + (amount/maxScore)*maxUp }, time);
+  hill.animate({ left: hillPos.left + (amount/maxScore)*maxRight }, time);
+  bkgd.animate({ left: hillPos.left + (amount/maxScore)*maxRight }, time);
+}
+
 function displayQuestion(){
   $('#questionText')[0].innerHTML = questions[currentQuestion][0];
-  $('#answer1')[0].innerHTML = questions[currentQuestion][1];
-  $('#answer2')[0].innerHTML = questions[currentQuestion][2];
-  $('#answer3')[0].innerHTML = questions[currentQuestion][3];
+  $('#answer1')[0].innerHTML = "&#163;" + questions[currentQuestion][1];
+  $('#answer2')[0].innerHTML = "&#163;" + questions[currentQuestion][2];
+  $('#answer3')[0].innerHTML = "&#163;" + questions[currentQuestion][3];
   currentQuestion += 1;
 }
